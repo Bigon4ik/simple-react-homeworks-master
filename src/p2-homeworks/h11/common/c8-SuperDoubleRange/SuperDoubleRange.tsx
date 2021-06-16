1b7b2158644f809b31b6273de126774ed1fd3815
Bigon4ik <bigon444ik@gmail.com>
2021-06-16 21:26:22 +0300

    add hw11

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Slider, Typography} from '@material-ui/core';
 
 type SuperDoubleRangePropsType = {
     onChangeRange?: (value: [number, number]) => void
@@ -13,10 +14,25 @@ const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     }
 ) => {
     // сделать самому, можно подключать библиотеки
-
+    const handleChange = (event: any, newValue: number | number[]) => {
+        setValue(newValue as number[]);
+    };
+    function valuetext(value: number) {
+        return `${value}°C`;
+    }
     return (
         <>
-            DoubleRange
+            {/*DoubleRange*/}
+            <Typography id="range-slider" gutterBottom>
+                Temperature range
+            </Typography>
+            <Slider
+                value={value}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                aria-labelledby="range-slider"
+                getAriaValueText={valuetext}
+            />
         </>
     )
 }
