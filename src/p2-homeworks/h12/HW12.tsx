abc10deb7262d87950b1818840bbd96d84942483
Bigon4ik <bigon444ik@gmail.com>
2021-06-21 19:34:56 +0300

    add hw12 dispatch and callback

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,25 +1,36 @@
-import React from "react";
-import s from "./HW12.module.css";
+import React from 'react';
+import s from './HW12.module.css';
 import {useDispatch, useSelector} from 'react-redux';
+import {AppStoreType} from '../h10/bll/store';
+import {changeThemeC} from './bll/themeReducer';
+import SuperRadio from '../h7/common/c6-SuperRadio/SuperRadio';
 
 const themes = ['dark', 'red', 'some'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
-const dispatch=useDispatch()
-const log=useSelector(()=>{})
+    //const theme = 'some'; // useSelector
+    const dispatch = useDispatch()
+    const theme = useSelector<AppStoreType,string>(state => state.themes.themes)
 
     // useDispatch, onChangeCallback
-const onChangeCallback=()=>{
+    const onChangeCallback = (value:string) => {
+dispatch(changeThemeC(value))
 
-
-}
+    }
     return (
         <div className={s[theme]}>
             <hr/>
             <span className={s[theme + '-text']}>
                 homeworks 12
             </span>
+            <div>
+                <SuperRadio
+                    value={theme}
+                    name={'radio'}
+                    options={themes}
+                    onChangeOption={onChangeCallback}
+                />
+            </div>
 
             {/*should work (должно работать)*/}
             {/*SuperSelect or SuperRadio*/}
